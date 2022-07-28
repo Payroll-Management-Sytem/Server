081cdf97c4c849c46a36e94b28b95518df22c79a
Tomin Joy <tominjk007@gmail.com>
2022-07-29 03:48:11 +0530

    bug fix

--- tsal-sum.php
+++ tsal-sum.php
@@ -11,6 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $net=($row[4]+(float)$row[5]*(float)$row[4]*0.01+(float)$row[6]*(float)$row[4]*0.01+(float)$row[7]*(float)$row[4]*0.01)-((float)$row[8]*(float)$row[4]*0.01+(float)$row[9]*(float)$row[4]*0.01+(float)$row[10]*(float)$row[4]*0.01);
             $string=$string.'<tr style="height: 48px;">
                 <td class="u-border-2 u-border-grey-dark-1 u-table-cell">'.$row[11].'  '.$row[12].'</td>
+                <td class="u-border-2 u-border-grey-dark-1 u-table-cell">'.$row[0].'</td>
+                <td class="u-border-2 u-border-grey-dark-1 u-table-cell">'.$row[3].'</td>
                 <td class="u-border-2 u-border-grey-dark-1 u-table-cell"> ₹'.$row[4].'</td>
                 <td class="u-border-2 u-border-grey-dark-1 u-table-cell"> ₹'.(float)$row[5]*(float)$row[4]*0.01.'</td>
                 <td class="u-border-2 u-border-grey-dark-1 u-table-cell"> ₹'.(float)$row[6]*(float)$row[4]*0.01.'</td>
