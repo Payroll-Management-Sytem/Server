a498a284d8e6b8b669946e867bceb72c0ef0b26e
Tomin Joy <tominjk007@gmail.com>
2022-07-28 18:39:34 +0530

    shammi entered chat

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-require "config.php";
-$query="SELECT `username`, `password` FROM `loginpage` WHERE 1";
-$result=mysqli_query($conn,$query);
-while ($row=mysqli_fetch_array($result))
-{
-echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>";
-}
+echo "<h1>entha mone hacking aano<h1><br><img src=\"./images/shammi.jpg\" alt=\"parayenne\">";
 ?>
