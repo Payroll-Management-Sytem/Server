c7b01734b99c7aac2d86c4ac5f320a81406cb2cc
Tomin Joy <tominjk007@gmail.com>
2022-07-29 11:36:35 +0530

    hi

--- new-emp.php
+++ new-emp.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require 'config.php';
-    foreach($_POST as $value){
-        echo $value."<br>";
-    }
+
+        echo $_POST['username']."<br>";
+    
 
 }else{
     echo "<h1>entha mone hacking aano<h1><br><img src=\"./images/shammi.jpg\" alt=\"parayenne\">";
