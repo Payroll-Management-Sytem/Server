bb475c5cf857275b8db8f3ed660de24f0d125dd0
Tomin Joy <tominjk007@gmail.com>
2022-07-29 12:31:42 +0530

    hi

--- new-emp.php
+++ new-emp.php
@@ -6,9 +6,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // foreach($_POST as $value){
         //     echo $value."<br>";
         // }
-        $query = "INSERT INTO `loginpage`(`uid`,`username`, `password`) VALUES (\"".$_POST['uid']."\",\"".$_POST['username']. "\",\"" . $_POST['pass'] . "\")";
-    if ($conn->query($query) === TRUE) {
-        echo "success";
+        $query = "INSERT INTO `loginpage`(`uid`,`username`, `empid`,`empname`,`dob`,`gender`,`dateofjoining`,`designation`,`office`) VALUES (\"".$_POST['uid']."\",\"".$_POST['username']. "\",\"" . $_POST['empid'] . "\",\"".$_POST['empname']. "\",\"".$_POST['age']. "\",\"".$_POST['dob']. "\",\"".$_POST['gender']. "\",\"".$_POST['doj']. "\",\"".$_POST['desig']. "\",\"".$_POST['ofc']. "\")";
+    if ($conn->query($query) === TRUE) { $query = "INSERT INTO `employeedetails`(`uid`,`username`, `password`) VALUES (\"".$_POST['uid']."\",\"".$_POST['username']. "\",\"" . $_POST['pass'] . "\")";
+        if ($conn->query($query) === TRUE) {
+            echo "success";
+        } else {
+            echo "Error:" . $conn->error;
+        }
     } else {
         echo "Error:" . $conn->error;
     }
