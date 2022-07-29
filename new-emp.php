5c89e4873eb356cc232e4c92f7a394f814e756a9
Tomin Joy <tominjk007@gmail.com>
2022-07-29 12:22:09 +0530

    test

--- new-emp.php
+++ new-emp.php
@@ -3,9 +3,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require 'config.php';
 
         // echo $_POST['username']."<br>";
-        foreach($_POST as $value){
-            echo $value."<br>";
-        }
+        // foreach($_POST as $value){
+        //     echo $value."<br>";
+        // }
+        $query = "INSERT INTO `loginpage`(`uid`,`username`, `password`) VALUES (\"".$_POST['uid']."\",\"".$_POST['username']. "\",\"" . $_POST['pass'] . "\")";
+    if ($conn->query($query) === TRUE) {
+        echo "success";
+    } else {
+        echo "Error:" . $conn->error;
+    }
 
 }else{
     echo "<h1>entha mone hacking aano<h1><br><img src=\"./images/shammi.jpg\" alt=\"parayenne\">";
