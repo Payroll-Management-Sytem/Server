1dd41b9ba0c32da25eda64893daacc6a414a40c5
Tomin Joy <tominjk007@gmail.com>
2022-08-01 14:44:36 +0530

    update fixed

--- update.php
+++ update.php
@@ -3,11 +3,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require 'config.php';
 
         
-        $query = "update `loginpage` set uid = ".$_POST['uid']."\", username= \"".$_POST['username']. "\", password=\"" . $_POST['pass'] . "\" where uid=\"" . $_POST['old'] ."\"";
+        $query = "update `loginpage` set 
+        `uid` = '".$_POST['uid']."',
+        `username` = '".$_POST['username']."',
+        `password` = '".$_POST['pass']."'
+        where `uid` = '".$_POST['old']."';";
     if ($conn->query($query) === TRUE) { 
-        $query1 = "update `employeedetails` set empid=" . $_POST['empid'] . "\", empname=\"".$_POST['empname']. "\", age = \"".$_POST['age']. "\", dob =\"".$_POST['dob']. "\", gender=\"".$_POST['gender']. "\", dateofjoining=\"".$_POST['doj']. "\", designation=\"".$_POST['desig']. "\", office=\"".$_POST['ofc']. "\" where uid =\"".$_POST['uid']."\"";
+        $query1 = "update `employeedetails` set 
+        `empid` = '" . $_POST['empid'] ."',
+        `empname`='".$_POST['empname']."',
+        `age` = '".$_POST['age']."',
+        `dob` = '".$_POST['dob']."',
+        `gender` = '".$_POST['gender']."',
+        `dateofjoining` = '".$_POST['doj']."',
+        `designation` = '".$_POST['desig']."',
+        `office`='".$_POST['ofc']."'
+        where `uid` ='".$_POST['uid']."';";
         if ($conn->query($query1) === TRUE) {
-            echo "success";
+            $query2="update `salarydetails` set
+            `empname` = '".$_POST['empname']."' where `uid` ='".$_POST['uid']."'";
+            if ($conn->query($query2) === TRUE) {
+                echo "success";
+            } else {
+                echo "Error:" . $conn->error;
+            }
         } else {
             echo "Error:" . $conn->error;
         }
@@ -16,6 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
 }else{
-    echo "<h1>entha mone hacking aano<h1><br><img src=\"./images/shammi.jpg\" alt=\"parayenne\">";
+    echo "<h1>entha mone hacking aano<h1><br><img src='./images/shammi.jpg' alt='parayenne'>";
 }
 ?>
